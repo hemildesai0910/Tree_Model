6347fd5d4fe24870f94600b7333f509c10431afc
Hemil Desai <147586274+hemildesai0910@users.noreply.github.com>
2025-03-05 22:42:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -45,4 +45,5 @@ def repo_tree():
     return jsonify(result)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Get PORT from environment variable, default to 5000
+    app.run(host="0.0.0.0", port=port, debug=True)
